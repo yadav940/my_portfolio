f08894408a7a2b794823b6cf3a9c0105bb171863
yadav940 <vinod.yadav940@gmail.com>
2025-03-02 09:48:30 +0530

    web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2206216457"
+    serviceWorkerVersion: "419600953"
   }
 });
